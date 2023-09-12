21c0a37629fa93fa92f588f68a16adc89d3e5003
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2023-09-12 16:10:48 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
 })
+*/
 
-/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -22,4 +22,3 @@ export default defineConfig(({ command }) => {
 
   return config
 })
-*/
